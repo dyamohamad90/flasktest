f42a38487c28cc5bf8ca3b79546ca7efb6575126
dyamohamad <nurdiyanamohamad@gmail.com>
2018-08-03 10:53:03 +0800

    update RDS config

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 # edit the URI below to add your RDS password and your AWS URL
 # The other elements are the same as used in the tutorial
-# format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
+# format: (user):(master password)@(db_identifier).amazonaws.com:3306/(db_name)
+#https://medium.com/@rodkey/deploying-a-flask-application-on-aws-a72daba6bb80
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:flasktest@flasktest.chdsnjoqqaid.ap-southeast-1.rds.amazonaws.com:3306/flaskdb'
+
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
