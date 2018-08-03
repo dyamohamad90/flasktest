82c8ae3a85626be2e6e532b9225ad8bb5fa116c1
dyamohamad <nurdiyanamohamad@gmail.com>
2018-08-03 16:32:26 +0800

    test

--- application.py
+++ application.py
@@ -22,8 +22,10 @@ application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
 @application.route('/index', methods=['GET', 'POST'])
 def index():
     form1 = EnterDBInfo(request.form) 
-    form2 = RetrieveDBInfo(request.form) 
-    
+    form2 = RetrieveDBInfo(request.form)
+
+
+
     if request.method == 'POST' and form1.validate():
         data_entered = Data(notes=form1.dbNotes.data)
         try:     
@@ -33,19 +35,26 @@ def index():
         except:
             db.session.rollback()
         return render_template('thanks.html', notes=form1.dbNotes.data)
+
         
     if request.method == 'POST' and form2.validate():
         try:   
             num_return = int(form2.numRetrieve.data)
             query_db = Data.query.order_by(Data.id.desc()).limit(num_return)
             for q in query_db:
-                print(q.notes + q.address)
+                print(q.notes)
             db.session.close()
         except:
             db.session.rollback()
-        return render_template('results.html', results=query_db, num_return=num_return)                
-    
-    return render_template('index.html', form1=form1, form2=form2)
+        return render_template('results.html', results=query_db, num_return=num_return)
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
--- application/models.py
+++ application/models.py
@@ -3,13 +3,11 @@ from application import db
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     notes = db.Column(db.String(128), index=True, unique=False)
-    address = db.Column(db.String(45), index=True, unique=False)
     
     def __init__(self, notes):
         self.notes = notes
 
-    def __init__(self, address):
-        self.address = address
+
 
     def __repr__(self):
-        return '<Data %r>' % self.notes  % self.address
+        return '<Data %r>' % self.notes
